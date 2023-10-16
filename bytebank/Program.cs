53ff2ca62cca678aacfefaff38278da338bd4779
juanprado <juan.prado@cacaushow.com.br>
2023-10-16 07:17:56 -0300

    Add project files.

---
+++ bytebank/Program.cs
@@ -0,0 +1,16 @@
+﻿using bytebank;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaCorrente contaDoJuan = new ContaCorrente();
+        contaDoJuan.titular = "Juan Prado";
+        contaDoJuan.numero_agencia = 18;
+        contaDoJuan.conta = "1010-X";
+        contaDoJuan.saldo = 100;
+
+        contaDoJuan.saldo = 200;
+        Console.WriteLine("Saldo da conta do Juan = " + contaDoJuan.saldo);
+    }
+}
