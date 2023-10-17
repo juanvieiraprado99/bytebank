1641aabd76397cf22b37f340d38eb235f28008d1
juanprado <juan.prado@cacaushow.com.br>
2023-10-17 07:14:07 -0300

    feat: tipos por referencia

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -4,45 +4,62 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        ContaCorrente contaDoJuan = new ContaCorrente();
-        ContaCorrente contaDaJulia = new ContaCorrente();
-        contaDaJulia.titular = "Julia Cristina";
-        contaDaJulia.numero_agencia = 23;
-        contaDaJulia.conta = "1010-5";
-        contaDaJulia.saldo = 350;
+        //ContaCorrente contadojuan = new ContaCorrente();
+        ContaCorrente contadajulia = new ContaCorrente();
+        contadajulia.titular = "julia cristina";
+        contadajulia.numero_agencia = 23;
+        contadajulia.conta = "1010-5";
+        contadajulia.saldo = 350;
 
-        contaDoJuan.titular = "Juan Prado";
-        contaDoJuan.numero_agencia = 18;
-        contaDoJuan.conta = "1010-X";
-        contaDoJuan.saldo = 100;
+        ContaCorrente contadajulia2 = new ContaCorrente();
+        contadajulia2.titular = "julia cristina";
+        contadajulia2.numero_agencia = 23;
+        contadajulia2.conta = "1010-5";
+        contadajulia2.saldo = 350;
 
-        Console.WriteLine("Saldo da conta do Juan = " + contaDoJuan.saldo);
+        Console.WriteLine(contadajulia == contadajulia2);
 
-        Console.WriteLine("Saldo da conta da Julia = " + contaDaJulia.saldo);
+        //contaDoJuan.titular = "Juan Prado";
+        //contaDoJuan.numero_agencia = 18;
+        //contaDoJuan.conta = "1010-X";
+        //contaDoJuan.saldo = 100;
+
+        //Console.WriteLine("Saldo da conta do Juan = " + contaDoJuan.saldo);
+
+        //Console.WriteLine("Saldo da conta da Julia = " + contaDaJulia.saldo);
 
 
-        contaDoJuan.Depositar(100);
-        Console.WriteLine("Saldo da conta do Juan pós-depósito = " + contaDoJuan.saldo);
+        //contaDoJuan.Depositar(100);
+        //Console.WriteLine("Saldo da conta do Juan pós-depósito = " + contaDoJuan.saldo);
 
-        if(contaDoJuan.Sacar(300))
-        {
-            Console.WriteLine("Saldo da conta do Juan depois do saque = " + contaDoJuan.saldo); 
-        }
-        else
-        {
-            Console.WriteLine("Saldo insuficiente para saque.");
-        }
+        //if(contaDoJuan.Sacar(300))
+        //{
+        //    Console.WriteLine("Saldo da conta do Juan depois do saque = " + contaDoJuan.saldo); 
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Saldo insuficiente para saque.");
+        //}
 
-        contaDoJuan.Transferir(50, contaDaJulia);
+        //contaDoJuan.Transferir(50, contaDaJulia);
 
-        Console.WriteLine("Saldo da conta do Juan depois de transferir = " + contaDoJuan.saldo);
+        //Console.WriteLine("Saldo da conta do Juan depois de transferir = " + contaDoJuan.saldo);
 
-        Console.WriteLine("Saldo da conta da Julia depois de receber transferencia = " + contaDaJulia.saldo);
+        //Console.WriteLine("Saldo da conta da Julia depois de receber transferencia = " + contaDaJulia.saldo);
 
-        ContaCorrente contaDoGael = new ContaCorrente();
-        Console.WriteLine(contaDoGael.titular);
-        Console.WriteLine(contaDoGael.saldo);
-        Console.WriteLine(contaDoGael.numero_agencia);
-        Console.WriteLine(contaDoGael.conta);
+        //ContaCorrente contaDoGael = new ContaCorrente();
+        //Console.WriteLine(contaDoGael.titular);
+        //Console.WriteLine(contaDoGael.saldo);
+        //Console.WriteLine(contaDoGael.numero_agencia);
+        //Console.WriteLine(contaDoGael.conta);
+
+        //double valor = 300;
+        //double valor2 = valor;
+
+        //Console.WriteLine(valor == valor2);
+
+        ContaCorrente conta = new ContaCorrente();
+
+
     }
 }
